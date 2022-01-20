dfe8afccf9375b13396bda39fc5b986bd0a3d088
xshouri <65828146+xshouri@users.noreply.github.com>
2022-01-20 19:49:04 +0100

    Zmiana strony statycznej

--- src/index.js
+++ src/index.js
@@ -3,21 +3,58 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
 
-function Board()
-{
-  return(
-    <div id='screen'>
-      <div id='board'>
-     
+class Screen extends React.Component {
+
+  render(){
+    return (
+      <div id='screen'>0</div>
+    )
+  }
+}
+
+class CalculatorButton extends React.Component {
+
+  render(){
+    let buttonClass = 'bttn';
+    if(this.props.bttnValue === "="){
+      buttonClass += ' bttn-equal';
+    }
+
+    return (
+        <button className={buttonClass}>{this.props.bttnValue}</button>
+    )
+  }
+}
+
+class Calculator extends React.Component {
+
+  render(){
+    const bttnValues = [
+      "CE", "C", "%", "/",
+      7, 8, 9, "X",
+      4, 5, 6, "-",
+      1, 2, 3, "+",
+      "<", 0, ".", "="
+    ];
+    const buttonsMap = bttnValues.map((bttnValue) =>
+      <CalculatorButton bttnValue = {bttnValue} />
+    );
+
+    return (
+      <div className='calculator'>
+        <Screen />
+        <div className='buttonsContainer'>
+          {buttonsMap}
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Board />
+    <Calculator />
   </React.StrictMode>,
   document.getElementById('root')
 );
